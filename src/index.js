b81148520fcef5d20bb825627e8feae575664c02
AlexandruARV <162844302+AlexandruARV@users.noreply.github.com>
2024-04-13 13:51:06 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -13,3 +13,42 @@ eyeImg.addEventListener("click", (e) => {
     inputPassword.type = "text";
   }
 });
+
+const formEl = document.querySelector(".form");
+console.log(formEl);
+let x = false;
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(formEl);
+  console.log(formData);
+
+  const data = Object.fromEntries(formData);
+  console.log(JSON.stringify(data));
+
+  async function sendData() {
+    try {
+      const sendSv = await fetch(
+        "https://server-63ql.onrender.com/receiveData",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (!sendSv.ok) throw new Error("Error to send data");
+
+      const res = await sendSv.json();
+      console.log(res);
+    } catch (error) {
+      console.error("An error apear:", error);
+    }
+
+    if (x) {
+      window.location.href = "http://127.0.0.1:5500/mainPage.html";
+    }
+  }
+  sendData();
+});
